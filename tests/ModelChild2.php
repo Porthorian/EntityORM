574a6b832831431a6a5c1f2e0a4874474ec2edf0
Vinnie Marone <vinnie.marone35@gmail.com>
2022-08-28 02:51:31 -0400

    Fix linter errors

--- tests/ModelChild2.php
+++ tests/ModelChild2.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Porthorian\EntityOrm\Tests;
 
-use Porthorian\EntityOrm\Model\Model;
-
 class ModelChild2 extends ModelChild
 {
 	//
--- tests/Suite/EntityTest.php
+++ tests/Suite/EntityTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Porthorian\EntityOrm\Tests;
 
 use PHPUnit\Framework\TestCase;
-
 use Porthorian\EntityOrm\Tests\EntityChild;
 use Porthorian\EntityOrm\Model\ModelInterface;
 use Porthorian\EntityOrm\Tests\ModelChild;
