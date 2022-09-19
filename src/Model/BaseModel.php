44ec1ab6f14c97c9f4678a6ae2d72d501705d625
Vinnie Marone <vinnie.marone35@gmail.com>
2022-09-18 23:58:21 -0400

    Remove reset happening inside constructor

--- src/Model/BaseModel.php
+++ src/Model/BaseModel.php
@@ -28,7 +28,6 @@ abstract class BaseModel implements BaseModelInterface
 	public function __construct()
 	{
 		$this->metadata = new ClassMetadata($this);
-		$this->reset();
 	}
 
 	public function toJSON() : string
